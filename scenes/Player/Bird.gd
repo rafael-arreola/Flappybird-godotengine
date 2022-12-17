e5c14b7ab74da6928c066ae085134343262bd849
Carlos Rafael Arreola Hernández <rafael-arreola@outlook.com>
2022-12-17 16:40:19 -0700

    Android export/Touch event/Cumulative gravity

--- scenes/Player/Bird.gd
+++ scenes/Player/Bird.gd
@@ -1,23 +1,29 @@
 extends CharacterBody2D
 
+signal player_start
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var started = false
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
-
-	# Handle flap.
+	
+	if velocity.y > SPEED * 2:
+		velocity.y = SPEED * 2
+		
 	if Input.is_action_just_pressed("flap"):
 		$AnimatedSprite2D.set_frame(0)
 		$AnimatedSprite2D.play()
 		velocity.y = JUMP_VELOCITY
+		if not started:
+			started = true
+			emit_signal("player_start")
 
 	var direction = 0.5
 	velocity.x = direction * SPEED
 	
-	move_and_slide()
+	if started:
+		move_and_slide()
--- scenes/world.gd
+++ scenes/world.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+func _on_bird_player_start():
+	$Message.hide()
+
+
+func _unhandled_input(event):
+	if event is InputEventScreenTouch:
+		Input.action_press("flap")
