0c46d0946cac3caa72f7dfb319bd0654e3e25bdd
Carlos Rafael Arreola Hernández <rafael-arreola@outlook.com>
2022-12-18 01:14:00 -0700

    Add pipes/fix velocity

--- scenes/Player/Bird.gd
+++ scenes/Player/Bird.gd
@@ -2,17 +2,14 @@ extends CharacterBody2D
 
 signal player_start
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -300.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var started = false
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
-	
-	if velocity.y > SPEED * 2:
-		velocity.y = SPEED * 2
 		
 	if Input.is_action_just_pressed("flap"):
 		$AnimatedSprite2D.set_frame(0)
@@ -22,8 +19,7 @@ func _physics_process(delta):
 			started = true
 			emit_signal("player_start")
 
-	var direction = 0.5
-	velocity.x = direction * SPEED
+	velocity.x = SPEED
 	
 	if started:
 		move_and_slide()
--- scenes/world.gd
+++ scenes/world.gd
@@ -1,9 +1,25 @@
 extends Node2D
 
+var MIN_Y_PIPES = 152.0
+var MAX_Y_PIPES = 360.0
+
+@export var pipes: PackedScene
+
 func _on_bird_player_start():
 	$Message.hide()
+	$Pipes/PipesSpawner.start()
+	_on_pipes_spawner_timeout()
 
 
 func _unhandled_input(event):
+	print(event)
 	if event is InputEventScreenTouch:
 		Input.action_press("flap")
+
+
+func _on_pipes_spawner_timeout():
+	var obstacle = pipes.instantiate()
+	var y_position = randf_range(MIN_Y_PIPES, MAX_Y_PIPES)
+	
+	obstacle.position = Vector2($Bird.position.x + 250, y_position)
+	$Pipes.add_child(obstacle)
